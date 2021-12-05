fb6d61ad7a6c8567a7c54b1dae5bfe15088714a9
Thomas Hansen <thomas@gaiasoul.com>
2021-12-05 08:57:44 +0200

    Removing structured parts to simplify API

--- magic.lambda.mime/MimeCreate.cs
+++ magic.lambda.mime/MimeCreate.cs
@@ -2,12 +2,9 @@
  * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
  */
 
-using System.Linq;
 using magic.node;
-using magic.node.extensions;
 using magic.signals.contracts;
 using magic.lambda.mime.helpers;
-using System.IO;
 
 namespace magic.lambda.mime
 {
@@ -24,37 +21,11 @@ namespace magic.lambda.mime
         /// <param name="input">Arguments to your slot.</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            // Figuring out if caller wants a structures result or not.
-            var structuredNode = input.Children.FirstOrDefault(x => x.Name == "structured");
-            var structured = structuredNode?.Get<bool>() ?? false;
-            structuredNode?.UnTie();
-
             // Creating entity
             var entity = MimeCreator.Create(signaler, input);
             try
             {
-                using (var stream = new MemoryStream())
-                {
-                    entity.WriteTo(stream, structured);
-                    input.Clear();
-                    stream.Position = 0;
-                    using (var reader = new StreamReader(stream))
-                    {
-                        var result = reader.ReadToEnd();
-                        if (structured)
-                        {
-                            var contentType = entity.ContentType.ToString();
-                            contentType = contentType.Substring(contentType.IndexOf(':') + 1).TrimStart();
-                            input.Add(new Node("Content-Type", contentType));
-                            input.Add(new Node("content", result));
-                            System.Console.WriteLine(input.ToHyperlambda());
-                        }
-                        else
-                        {
-                            input.Value = result;
-                        }
-                    }
-                }
+                input.Value = entity.ToString();
             }
             finally
             {
